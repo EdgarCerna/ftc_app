00c557918bce345548e291854d026856b8300d05
Ibrahim Ayyad <ibrahimayyad@Ibrahims-MacBook-Pro.local>
2018-09-22 14:13:21 -0500

    Added SmallBotHardware & SmallBotDriverControlled

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpeedyGonzalez/EdgarDriverControlled.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpeedyGonzalez/EdgarDriverControlled.java
@@ -1,9 +1,7 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.SpeedyGonzalez;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="EdgarDriverControlled", group="TeleOp")
 
