1c5faf4e27e1eee588ca48f90c0a5d8facc82fba
Edgar <cerna_edgar@yahoo.com>
2018-09-30 11:41:50 -0500

    Added GyroSensor gyro to CompetitionHardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionRobot/CompetitionHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionRobot/CompetitionHardware.java
@@ -3,14 +3,18 @@ package org.firstinspires.ftc.teamcode.CompetitionRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.GyroSensor;
 
 public class CompetitionHardware
 {
     //INSTANTIATE MOTORS
-    public DcMotor frontLeftDrive;
-    public DcMotor frontRightDrive;
-    public DcMotor rearLeftDrive;
-    public DcMotor rearRightDrive;
+    public DcMotor frontLeftDrive = null;
+    public DcMotor frontRightDrive = null;
+    public DcMotor rearLeftDrive = null;
+    public DcMotor rearRightDrive = null;
+
+    //INSTANTIATE SENSORS
+    public GyroSensor gyro;
 
     // Local OpMode members
     HardwareMap hardwareMap;
@@ -23,6 +27,9 @@ public class CompetitionHardware
         frontRightDrive = hardwareMap.get(DcMotor.class, "frontRightDrive");
         rearLeftDrive = hardwareMap.get(DcMotor.class, "rearLeftDrive");
         rearRightDrive = hardwareMap.get(DcMotor.class, "rearRightDrive");
+
+        //DEFINE SENSORS
+        gyro = hardwareMap.get(GyroSensor.class, "imu");
         
         //SET MOTOR DIRECTION
         frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
@@ -46,5 +53,8 @@ public class CompetitionHardware
         frontRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rearLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rearRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //CALIBRATE SENSORS
+        gyro.calibrate();
     }
 }
