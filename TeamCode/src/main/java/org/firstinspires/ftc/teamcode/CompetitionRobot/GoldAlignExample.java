377d05ef79490245377b1008eeefb690d35678fe
Cerna <S1460902@student.mcckc.edu>
2018-10-09 19:22:21 -0500

    GoldAlignExample should turn til gold cube is in center, then drive straight 24 inches (not tested)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionRobot/GoldAlignExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionRobot/GoldAlignExample.java
@@ -4,19 +4,35 @@ import com.disnodeteam.dogecv.CameraViewDisplay;
 import com.disnodeteam.dogecv.DogeCV;
 import com.disnodeteam.dogecv.detectors.roverrukus.GoldAlignDetector;
 import com.disnodeteam.dogecv.detectors.roverrukus.SamplingOrderDetector;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
-
-@TeleOp(name="GoldAlign Example", group="DogeCV")
+@Autonomous(name="GoldAlign Example", group="Autonomous")
 
 public class GoldAlignExample extends OpMode
 {
     private GoldAlignDetector detector;
+    CompetitionHardware robot = new CompetitionHardware();
 
+    static final double     COUNTS_PER_MOTOR_REV    = 2240 ;    // eg: TETRIX Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 3.54331 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+                                                        (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double     DRIVE_SPEED             = 0.2;
+    static final double     TURN_SPEED              = 0.1;
 
     @Override
     public void init() {
+        robot.init(hardwareMap);
+
+        robot.frontLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.frontRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        robot.frontLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.frontRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
         telemetry.addData("Status", "DogeCV 2018.0 - Gold Align Example");
 
         detector = new GoldAlignDetector();
@@ -55,6 +71,28 @@ public class GoldAlignExample extends OpMode
 
     @Override
     public void loop() {
+        final int target = 320;
+        int centerCnt = 0;
+        double leftPower;
+        double rightPower;
+
+        while (centerCnt < 3) {
+            if (detector.getXPosition() < 320) {
+                robot.frontLeftDrive.setPower(TURN_SPEED);
+                robot.frontRightDrive.setPower(-TURN_SPEED);
+            }
+            else if (detector.getXPosition() > 320) {
+                robot.frontLeftDrive.setPower(-TURN_SPEED);
+                robot.frontRightDrive.setPower(TURN_SPEED);
+            }
+            else if (detector.getXPosition() == 320) {
+                centerCnt++;
+            }
+        }
+        if (detector.getAligned() == true) {
+            encoderDrive(DRIVE_SPEED, 24, 24);
+        }
+
         telemetry.addData("IsAligned" , detector.getAligned()); // Is the bot aligned with the gold mineral
         telemetry.addData("X Pos" , detector.getXPosition()); // Gold X pos.
     }
@@ -65,6 +103,36 @@ public class GoldAlignExample extends OpMode
     @Override
     public void stop() {
         detector.disable();
+        robot.frontLeftDrive.setPower(0);
+        robot.frontRightDrive.setPower(0);
+    }
+
+    public void encoderDrive(double speed,
+                             double leftInches, double rightInches) {
+        int newLeftTarget;
+        int newRightTarget;
+
+        // Determine new target position, and pass to motor controller
+        newLeftTarget = robot.frontLeftDrive.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+        newRightTarget = robot.frontRightDrive.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+        robot.frontLeftDrive.setTargetPosition(newLeftTarget);
+        robot.frontRightDrive.setTargetPosition(newRightTarget);
+
+        // Turn On RUN_TO_POSITION
+        robot.frontLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.frontRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // reset the timeout time and start motion.
+        robot.frontLeftDrive.setPower(Math.abs(speed));
+        robot.frontRightDrive.setPower(Math.abs(speed));
+
+        // Stop all motion;
+        robot.frontLeftDrive.setPower(0);
+        robot.frontRightDrive.setPower(0);
+
+        // Turn off RUN_TO_POSITION
+        robot.frontLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.frontRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
 }
